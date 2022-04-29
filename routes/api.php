7fa257080ec76e4cdfa854465e7c709d89fef191
NS <alijeyar765@gmail.com>
2022-04-29 23:47:24 +0300

    authentication test

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('createaccount',[\App\Http\Controllers\AuthController::class, 'createAccount']);
+Route::post('loginaccount', [\App\Http\Controllers\AuthController::class, 'login']);
+
