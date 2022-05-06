53b7196c6782acc3b8e9b9aca6083b9fa0f1458b
NS <alijeyar765@gmail.com>
2022-05-06 20:49:50 +0300

    items show and update endpoint

--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -75,9 +75,16 @@ class TradeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request, $id)
     {
-        //
+        $user = $request->user();
+        $profile = Profile::where('user_id',$user->id)->first();
+        $trade = Trade::find($id);
+        if ($trade->itemSendObject->profile->id == $profile->id || $trade->itemRecieveObject->profile->id == $profile->id)
+        {
+            return response()->json($trade,200);
+        }
+        return response()->json("unauthorized",403);
     }
 
     /**
@@ -87,9 +94,39 @@ class TradeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,TradeRequest $payload, $id)
     {
-        //
+        $user = $request->user();
+        $profile = Profile::where('user_id',$user->id)->first();
+        $itemSend = Item::find($payload->itemSend);
+
+        $itemReceive = Item::find($payload->itemReceive);
+
+        //check if item exists
+        if (!$itemSend || !$itemReceive){
+            return response()->json("item doesn't exist",404);
+        }
+        //check if the item you are sending is yours
+        if ($itemSend->profile->id != $profile->id)
+        {
+            return response()->json('this is not your item',400);
+        }
+        //check if the item you taking is not yours
+        if ($itemReceive->profile->id == $profile->id)
+        {
+            return response()->json('dont trade with your own inventory',400);
+        }
+        $trade = Trade::find($id);
+        //check if confirmation by the received user
+        if($trade->confirmation == $profile->id)
+        {
+            $trade['itemSend'] = $itemSend->id;
+            $trade['itemReceive '] = $itemReceive->id;
+            $trade['confirmation'] = $itemReceive->profile->id;
+            $trade->save();
+            return response()->json($trade,201);
+        }
+        return response()->json("unauthorized",403);
     }
 
     /**
